590a674aacb802566bf9723b40ed81dfbb44a4cf
nanchengcyu <1146137024@qq.com>
2023-10-29 01:34:04 +0800

    2022-10-29 Java study

---
+++ src/Hello.java
@@ -0,0 +1,5 @@
+import java.util.Scanner;
+
+public class Hello {
+    Scanner scanner =new Scanner(System.in);
+}
---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * ClassName: Test
+ * Package: PACKAGE_NAME
+ * Description:这是一个测试
+ *
+ * @Author 南城余
+ * @Create 2023/10/29 0:57
+ * @Version 1.0
+ */
+public class Test {
+}
