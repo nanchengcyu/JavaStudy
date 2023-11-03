7b2ba546c846ef90cd34f3f7eaf3a4476a615cd6
nanchengcyu <1146137024@qq.com>
2023-11-04 00:11:41 +0800

    extends test

---
+++ oppordinary/src/cn/nanchengyu/extend/test/Circle.java
@@ -0,0 +1,9 @@
+package cn.nanchengyu.extend.test;/**
+* ClassName: Circle
+* Package: cn.nanchengyu.extend.test
+* Description: 
+* @Author 南城余
+* @Create 2023/11/3 23:50 
+* @Version 1.0   
+*/public class Circle {
+}
---
+++ oppordinary/src/cn/nanchengyu/extend/test/Test.java
@@ -0,0 +1,9 @@
+package cn.nanchengyu.extend.test;/**
+* ClassName: Test
+* Package: cn.nanchengyu.extend.test
+* Description: 
+* @Author 南城余
+* @Create 2023/11/3 23:57 
+* @Version 1.0   
+*/public class Test {
+}
