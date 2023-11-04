ef2757bc68c15e9093d9708d40efc9c6858b4fa9
nanchengcyu <1146137024@qq.com>
2023-11-04 22:15:05 +0800

    equals学习

---
+++ oppordinary/src/cn/nanchengyu/equals/Order.java
@@ -0,0 +1,15 @@
+package cn.nanchengyu.equals;
+
+/**
+ * ClassName: Order
+ * Package: cn.nanchengyu.equals
+ * Description:
+ *
+ * @Author 南城余
+ * @Create 2023/11/4 22:06
+ * @Version 1.0
+ */
+public class Order {
+    int orderId;
+    
+}
---
+++ oppordinary/src/cn/nanchengyu/equals/Test.java
@@ -0,0 +1,9 @@
+package cn.nanchengyu.equals;/**
+* ClassName: Test
+* Package: cn.nanchengyu.equals
+* Description: 
+* @Author 南城余
+* @Create 2023/11/4 22:09 
+* @Version 1.0   
+*/public class Test {
+}
