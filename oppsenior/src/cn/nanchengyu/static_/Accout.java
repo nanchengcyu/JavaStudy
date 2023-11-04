e938b624331262950bc7f94e7e1bfe589902ac23
nanchengcyu <1146137024@qq.com>
2023-11-05 01:25:45 +0800

    static学习

---
+++ oppsenior/src/cn/nanchengyu/static_/Accout.java
@@ -0,0 +1,9 @@
+package cn.nanchengyu.static_;/**
+* ClassName: Accout
+* Package: cn.nanchengyu.static_
+* Description: 
+* @Author 南城余
+* @Create 2023/11/5 0:19 
+* @Version 1.0   
+*/public class Accout {
+}
---
+++ oppsenior/src/cn/nanchengyu/static_/AccoutTest.java
@@ -0,0 +1,16 @@
+package cn.nanchengyu.static_;
+
+/**
+ * ClassName: AccoutTest
+ * Package: cn.nanchengyu.static_
+ * Description:
+ *
+ * @Author 南城余
+ * @Create 2023/11/5 0:32
+ * @Version 1.0
+ */
+public class AccoutTest {
+    public static void main(String[] args) {
+        
+    }
+}
