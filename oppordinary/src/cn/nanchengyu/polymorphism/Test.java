ad8b293d1a1cf316369b5dbcb6a66f6519e44db5
nanchengcyu <1146137024@qq.com>
2023-11-04 21:04:29 +0800

    多态学习

---
+++ oppordinary/src/cn/nanchengyu/polymorphism/Test.java
@@ -0,0 +1,9 @@
+package cn.nanchengyu.polymorphism;/**
+* ClassName: Test
+* Package: cn.nanchengyu.polymorphism
+* Description: 
+* @Author 南城余
+* @Create 2023/11/4 20:14 
+* @Version 1.0   
+*/public class Test {
+}
