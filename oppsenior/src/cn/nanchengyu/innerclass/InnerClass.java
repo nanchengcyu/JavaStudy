2a8d712a1360b25d121de90672340fbbb0f05c76
nanchengcyu <1146137024@qq.com>
2023-11-05 21:28:42 +0800

    内部类和抽象类和接口匿名调用方法  study

---
+++ oppsenior/src/cn/nanchengyu/innerclass/InnerClass.java
@@ -0,0 +1,9 @@
+package cn.nanchengyu.innerclass;/**
+* ClassName: InnerClass
+* Package: cn.nanchengyu.innerclass
+* Description: 
+* @Author 南城余
+* @Create 2023/11/5 20:50 
+* @Version 1.0   
+*/public class InnerClass {
+}
